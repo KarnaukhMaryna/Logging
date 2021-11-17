16122064ce59fbc6c7aa2a337ab60ea2e8fe52f9
Maryna Karnaukh <karnaukh.m.a.97@gmail.com>
2021-11-17 15:59:25 +0200

    New commit

--- LINQ/LINQ/Classes/OperationsWithFiles.cs
+++ LINQ/LINQ/Classes/OperationsWithFiles.cs
@@ -12,7 +12,7 @@ namespace LINQ.Classes
             string timeStamp = DateTime.Now.ToFileTime().ToString();
             string filename = "input_" + timeStamp;
 
-            using (StreamWriter streamWriterTxt = new StreamWriter($"{directory}{filename}.txt"))
+            using (StreamWriter streamWriterTxt = new StreamWriter($"{directory}\\{filename}.txt"))
             {
                 foreach (var ar in array)
                 {
@@ -25,7 +25,7 @@ namespace LINQ.Classes
 
         public static void ReadData(DirectoryInfo directory, string filename, char symbolTxt, ref List<int> oddList, ref List<int> evenList, ref List<int> equalList)
         {
-            using (StreamReader streamReaderTxt = new StreamReader($"{directory}{filename}.txt"))
+            using (StreamReader streamReaderTxt = new StreamReader($"{directory}\\{filename}.txt"))
             {
                 DefineTypeOfNumbers.TypeOfNumbers(streamReaderTxt, symbolTxt, ref oddList, ref evenList, ref equalList);
             }
